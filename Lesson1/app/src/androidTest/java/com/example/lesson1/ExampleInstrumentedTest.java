f97a569ea71f031f306fd05ce67dc5783c5a4f96
sozidatel319 <mbogat@yandex.ru>
2019-12-01 14:51:57 +0300

    Начало потока

--- Lesson1/app/src/androidTest/java/com/example/lesson1/ExampleInstrumentedTest.java
+++ Lesson1/app/src/androidTest/java/com/example/lesson1/ExampleInstrumentedTest.java
@@ -1,7 +1,6 @@
 package com.example.lesson1;
 
 import android.content.Context;
-
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
